c67ab8b373817f80c99b7dbf1512a3738da054e9
XueJingSa <2711206251@qq.com>
2025-04-17 00:08:59 +0800

    3

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,21 +1,65 @@
-
 <template>
-    <div>
-    <header>
-      <h1>我是导航栏</h1>
-    </header>
-    <router-view />
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
   </div>
 </template>
+
 <script>
-
 export default {
-  components: {
-
+  name: 'NavHeader',
+  data() {
+    return {
+      activeMenu: '1'
+    }
+  },
+  methods: {
+    goToHome() {
+      this.$router.push('/home')
+      this.activeMenu = '1'
+    },
+    goToCart() {
+      this.$router.push('/cart')
+      this.activeMenu = '2'
+    },
+    goToProfile() {
+      this.$router.push('/profile')
+      this.activeMenu = '3'
+    }
   }
 }
 </script>
 
 <style scoped>
+.nav-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
+.logo {
+  display: flex;
+  align-items: center;
+}
+
+.logo-img {
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+}
+
+.logo-title {
+  font-size: 20px;
+  color: #ffffff;
+  margin: 0;
+}
+
+.nav-menu {
+  /* 文本右对齐 */
+  text-align: right;
+}
 </style>
